e4d5e5fedcca5af3aac6cbc649c4aabb53d80d94
Guillaume <guillaumeilboudo078@gmail.com>
2023-06-05 16:23:08 +0200

    Fichier .py sur les variables

---
+++ Tuto_1/#-2 Variable/Script_variable.py
@@ -0,0 +1,16 @@
+#### Déclaration de variable
+
+nom_utilisateur = "Guilaume"
+
+age = 7
+
+###  Affichage
+
+print(nom_utilisateur, age + " ans")
+
+### Assigantion multiple
+nom, Prenom, universite, age, moyenne = "Ilboudo", "Guillaume", "Aube nouvelle", 2, 18.9
+
+#### Affichage 
+
+print(f"Je me nomme {nom} {Prenom} Etudiant à {universite} j'ai {age} au premier semestre {moyenne}") 
