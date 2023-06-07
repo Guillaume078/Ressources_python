737b26295e2fd5252bcdeb1117dc6c34abf5f9d3
Guillaume <guillaumeilboudo078@gmail.com>
2023-06-07 10:55:54 +0200

    Script modularité

---
+++ Tuto_1/#-7 Modularité/Script_modularié.py
@@ -0,0 +1,15 @@
+# from math import *
+# import math
+from math import sqrt
+
+print(sqrt(4))
+
+
+#### importer son propre module 
+
+from  include.player  import *
+au_revoir()
+parler("Guillaume", "Bonjour comment allez-vous ?")
+
+
+
