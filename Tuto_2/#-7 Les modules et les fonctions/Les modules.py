5987a1b4e2508df35773e532551c06d3f7f9792c
Guillaume <guillaumeilboudo078@gmail.com>
2023-08-28 16:37:04 +0100

    Tuto_2 Les modules et les fonctions

---
+++ Tuto_2/#-7 Les modules et les fonctions/Les modules.py
@@ -0,0 +1,16 @@
+# le module Random
+import random
+
+# la fonction randint
+r = random.randint(0, 1) #la valeur est inclusive
+print(r)
+
+# la fonction uniform
+
+r = random.uniform(0,1)
+print(r)
+
+# la fonction randrange
+
+r =  random.randrange(2) #la valeur est exclusive (randrange(0, 3, 15) avec trois comme pas)
+print (r)
